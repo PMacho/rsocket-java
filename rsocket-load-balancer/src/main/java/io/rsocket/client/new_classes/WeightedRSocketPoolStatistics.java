39b8ed6e0602279ef13c3ea82733ae2b46b2e69c
Peter Machon <machon.peter@yahoo.de>
2020-05-03 21:11:36 +0200

    probably found working concurrency model

---
+++ rsocket-load-balancer/src/main/java/io/rsocket/client/new_classes/WeightedRSocketPoolStatistics.java
@@ -0,0 +1,57 @@
+package io.rsocket.client.new_classes;
+
+import io.rsocket.stat.FrugalQuantile;
+
+public class WeightedRSocketPoolStatistics {
+
+    static final double DEFAULT_LOWER_QUANTILE = 0.2;
+    static final double DEFAULT_HIGHER_QUANTILE = 0.8;
+
+    private final Quantiles quantiles;
+
+//    final Consumer<Double> quantilesUpdater;
+//    final Supplier<LoadBalancingStatistics.Quantiles> quantilesSupplier;
+
+//    private ConcurrentOperations<LoadBalancingStatistics> operations;
+
+    WeightedRSocketPoolStatistics() {
+        this.quantiles = new Quantiles(DEFAULT_LOWER_QUANTILE, DEFAULT_HIGHER_QUANTILE);
+
+//        quantilesUpdater = this::updateQuantiles;
+//        quantilesSupplier = this::getQuantiles;
+//        this.operations = new ConcurrentOperations<>(this);
+    }
+
+    public void updateQuantiles(double rtt) {
+        quantiles.update(rtt);
+    }
+
+    public Quantiles getQuantiles() {
+        return quantiles;
+    }
+
+    static class Quantiles {
+        private FrugalQuantile lowerQuantile;
+        private FrugalQuantile higherQuantile;
+
+        Quantiles(double initialLowerQuantile, double initialHigherQuantile) {
+            this.lowerQuantile = new FrugalQuantile(initialLowerQuantile);
+            this.higherQuantile = new FrugalQuantile(initialHigherQuantile);
+        }
+
+        WeightedRSocketPoolStatistics.Quantiles update(double rtt) {
+            lowerQuantile.insert(rtt);
+            higherQuantile.insert(rtt);
+            return this;
+        }
+
+        double getLowerQuantile() {
+            return lowerQuantile.estimation();
+        }
+
+        double getHigherQuantile() {
+            return higherQuantile.estimation();
+        }
+    }
+
+}
